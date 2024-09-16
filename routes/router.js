c6599a96a5df3a716057c8f976d7c06a862fb4eb
Navawee <t-nawawee.mal@axons.in.th>
2024-09-17 00:54:45 +0700

    routes

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,20 @@
-const { login } = require("../controllers/loginController");
+const { login, addUser, updateUser, userAll, deleteUser } = require("../controllers/loginController");
 // const { checkAuth } = require("../middleware/checkAuth");
 const router = require("express").Router();
 
 module.exports = (app) => {
-//   router.post("/register",checkAuth, register);
+  //   router.post("/register",checkAuth, register);
   router.post("/login", login);
-//   router.post("/checkAuth", checkAuth);
-//   router.get("/getUser", getUser);
+  //   router.post("/checkAuth", checkAuth);
+  //   router.get("/getUser", getUser);
 
-  
+  router.post("/addUser", addUser);
+
+  router.post("/updateUser", updateUser)
+
+  router.get("/userAll", userAll)
+
+  router.post("/deleteUser", deleteUser)
+
   app.use("/api", router);
 };
