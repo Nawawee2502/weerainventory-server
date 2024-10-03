6d0d8cd78d831e0919bb54e5bbd5e8b9b42126be
Navawee <t-nawawee.mal@axons.in.th>
2024-10-03 14:42:33 +0700

    coderoutes

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,10 @@
 const { login, addUser, updateUser, userAll, deleteUser } = require("../controllers/loginController");
 const { addtypeproduct, updatetypeproduct, deletetypeproduct, typeproductAll,counttypeProduct, searchtypeProductName, typeproductcode} = require("../controllers/tbl_typeproductController")
-const { addunit, updateunit, deleteunit, unitAll,countUnit} = require("../controllers/tbl_unitController")
-const { addproduct, updateproduct, deleteproduct, productAll,countProduct} = require("../controllers/tbl_productController")
-const { addbranch, updatebranch, deletebranch, branchAll,countBranch} = require("../controllers/tbl_branchModel")
-const { addkitchen, updatekitchen, deletekitchen, kitchenAll,countKitchen} = require("../controllers/tbl_kitchenController")
-const { addsupplier, updatesupplier, deletesupplier, supplierAll,countSupplier} = require("../controllers/tbl_supplierController")
+const { addunit, updateunit, deleteunit, unitAll,countUnit, searchUnitName, unitcode} = require("../controllers/tbl_unitController")
+const { addproduct, updateproduct, deleteproduct, productAll,countProduct, searchProductName, productcode} = require("../controllers/tbl_productController")
+const { addbranch, updatebranch, deletebranch, branchAll,countBranch, searchBranchName, branchcode} = require("../controllers/tbl_branchModel")
+const { addkitchen, updatekitchen, deletekitchen, kitchenAll,countKitchen, searchKitchenName, kitchencode} = require("../controllers/tbl_kitchenController")
+const { addsupplier, updatesupplier, deletesupplier, supplierAll,countSupplier, searchSupplierName, suppliercode} = require("../controllers/tbl_supplierController")
 const { addtypeuser, updatetypeuser, deletetypeuser, typeuserAll,counttypeUser} = require("../controllers/tbl_typeuserController")
 const { addtypeuserpermission, updatetypeuserpermission, deletetypeuserpermission, typeuserpermissionAll,countTypeuserpermission} = require("../controllers/tbl_typeuserpermissionController")
 
@@ -37,6 +37,8 @@ module.exports = (app) => {
   router.post("/deleteunit",deleteunit)
   router.post("/unitall",unitAll)
   router.post("/countunit",countUnit)
+  router.post("/searchunitname", searchUnitName)
+  router.post("/unitcode", unitcode)
 
   // product
   router.post("/addproduct",addproduct)
@@ -44,6 +46,8 @@ module.exports = (app) => {
   router.post("/deleteproduct",deleteproduct)
   router.post("/productall",productAll)
   router.post("/countproduct",countProduct)
+  router.post("/searchproductname", searchProductName)
+  router.post("/productcode", productcode)
 
   // branch
   router.post("/addbranch",addbranch)
@@ -51,6 +55,8 @@ module.exports = (app) => {
   router.post("/deletebranch",deletebranch)
   router.post("/branchall",branchAll)
   router.post("/countbranch",countBranch)
+  router.post("/searchbranchname", searchBranchName)
+  router.post("/branchcode", branchcode)
 
   // kitchen
   router.post("/addkitchen",addkitchen)
@@ -58,6 +64,8 @@ module.exports = (app) => {
   router.post("/deletekitchen",deletekitchen)
   router.post("/kitchenall",kitchenAll)
   router.post("/countKitchen",countKitchen)
+  router.post("/searchkitchenname", searchKitchenName)
+  router.post("/kitchencode", kitchencode)
 
   // supplier
   router.post("/addsupplier",addsupplier)
@@ -65,6 +73,8 @@ module.exports = (app) => {
   router.post("/deletesupplier",deletesupplier)
   router.post("/supplierall",supplierAll)
   router.post("/countSupplier",countSupplier)
+  router.post("/searchSuppliername", searchSupplierName)
+  router.post("/suppliercode", suppliercode)
 
   // type_user
   router.post("/addtypeuser",addtypeuser)
